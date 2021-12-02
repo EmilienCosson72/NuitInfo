f1b01bf291765e0277cb634a11b5f08840462ed4
EmilienCosson72 <95433362+EmilienCosson72@users.noreply.github.com>
2021-12-02 21:01:57 +0100

    Ajout du fichier sql d'installation de la BDD

---
+++ bdd_sauveteurs_en_mer.sql
@@ -0,0 +1,147 @@
+DROP DATABASE IF EXISTS nuitdelinfo;
+CREATE DATABASE nuitdelinfo;
+
+USE nuitdelinfo;
+
+CREATE TABLE UTILISATEUR(
+   Id_User VARCHAR(50),
+   Email_User VARCHAR(50) NOT NULL,
+   Password_User VARCHAR(100) NOT NULL,
+   Nom_Role VARCHAR(50) NOT NULL,
+   Pseudo_User VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_User)
+);
+
+CREATE TABLE CATEGORIE(
+   Id_Categorie VARCHAR(50),
+   Nom_Categorie VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Categorie)
+);
+
+CREATE TABLE BATEAU_CONTRIBUTION(
+   Id_Bateau VARCHAR(50),
+   Nom_Bateau VARCHAR(50) NOT NULL,
+   Dimension_Bateau VARCHAR(25) NOT NULL,
+   Desc_Bateau VARCHAR(5000),
+   Est_Refuse BOOLEAN NOT NULL,
+   Id_User VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Bateau),
+   FOREIGN KEY(Id_User) REFERENCES UTILISATEUR(Id_User)
+);
+
+CREATE TABLE VICTIME_CONTRIBUTION(
+   Id_Vict VARCHAR(50),
+   Nom_Vict VARCHAR(50) NOT NULL,
+   Prenom_Vict VARCHAR(50) NOT NULL,
+   Ville_Vict VARCHAR(50),
+   Date_Naissance_Vict VARCHAR(50),
+   Sexe_Vict VARCHAR(1),
+   Est_Refuse BOOLEAN NOT NULL,
+   Id_User VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Vict),
+   FOREIGN KEY(Id_User) REFERENCES UTILISATEUR(Id_User)
+);
+
+CREATE TABLE INCIDENT_CONTRIBUTION(
+   Id_Incident VARCHAR(50),
+   Date_Incident DATETIME NOT NULL,
+   Localisation_Incident VARCHAR(50) NOT NULL,
+   Desc_Incident VARCHAR(5000),
+   Est_Refuse BOOLEAN NOT NULL,
+   Id_User VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Incident),
+   FOREIGN KEY(Id_User) REFERENCES UTILISATEUR(Id_User)
+);
+
+CREATE TABLE SAUVETEUR_CONTRIBUTION(
+   Id_Sauv VARCHAR(50),
+   Nom_Sauv VARCHAR(50) NOT NULL,
+   Prenom_Sauv VARCHAR(50) NOT NULL,
+   Ville_Sauv VARCHAR(50),
+   Date_Naissance_Sauv DATE,
+   Sexe_Sauv VARCHAR(1),
+   Est_Refuse BOOLEAN NOT NULL,
+   Id_User VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Sauv),
+   FOREIGN KEY(Id_User) REFERENCES UTILISATEUR(Id_User)
+);
+
+CREATE TABLE SAUVETEUR(
+   Id_Sauv VARCHAR(50),
+   Nom_Sauv VARCHAR(50) NOT NULL,
+   Prenom_Sauv VARCHAR(50) NOT NULL,
+   Ville_Sauv VARCHAR(50),
+   Date_Naissance_Sauv DATE,
+   Sexe_Sauv VARCHAR(1),
+   Id_User VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Sauv),
+   FOREIGN KEY(Id_User) REFERENCES UTILISATEUR(Id_User)
+);
+
+CREATE TABLE INCIDENT(
+   Id_Incident VARCHAR(50),
+   Date_Incident DATETIME NOT NULL,
+   Localisation_Incident VARCHAR(50) NOT NULL,
+   Desc_Incident VARCHAR(5000),
+   Id_User VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Incident),
+   FOREIGN KEY(Id_User) REFERENCES UTILISATEUR(Id_User)
+);
+
+CREATE TABLE VICTIME(
+   Id_Vict VARCHAR(50),
+   Nom_Vict VARCHAR(50) NOT NULL,
+   Prenom_Vict VARCHAR(50) NOT NULL,
+   Ville_Vict VARCHAR(50),
+   Date_Naissance_Vict VARCHAR(50),
+   Sexe_Vict VARCHAR(1),
+   Id_User VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Vict),
+   FOREIGN KEY(Id_User) REFERENCES UTILISATEUR(Id_User)
+);
+
+CREATE TABLE BATEAU(
+   Id_Bateau VARCHAR(50),
+   Nom_Bateau VARCHAR(50) NOT NULL,
+   Dimension_Bateau VARCHAR(25) NOT NULL,
+   Desc_Bateau VARCHAR(5000),
+   Id_User VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Bateau),
+   FOREIGN KEY(Id_User) REFERENCES UTILISATEUR(Id_User)
+);
+
+CREATE TABLE ARTICLE(
+   Id_Article VARCHAR(50),
+   Contenu_Article VARCHAR(50) NOT NULL,
+   Desc_Article VARCHAR(50) NOT NULL,
+   Date_Article DATETIME NOT NULL,
+   Id_Categorie VARCHAR(50) NOT NULL,
+   Id_User VARCHAR(50) NOT NULL,
+   PRIMARY KEY(Id_Article),
+   FOREIGN KEY(Id_Categorie) REFERENCES CATEGORIE(Id_Categorie),
+   FOREIGN KEY(Id_User) REFERENCES UTILISATEUR(Id_User)
+);
+
+CREATE TABLE INTERVENTION(
+   Id_Sauv VARCHAR(50),
+   Id_Incident VARCHAR(50),
+   PRIMARY KEY(Id_Sauv, Id_Incident),
+   FOREIGN KEY(Id_Sauv) REFERENCES SAUVETEUR(Id_Sauv),
+   FOREIGN KEY(Id_Incident) REFERENCES INCIDENT(Id_Incident)
+);
+
+CREATE TABLE DEGAT(
+   Id_Incident VARCHAR(50),
+   Id_Vict VARCHAR(50),
+   PRIMARY KEY(Id_Incident, Id_Vict),
+   FOREIGN KEY(Id_Incident) REFERENCES INCIDENT(Id_Incident),
+   FOREIGN KEY(Id_Vict) REFERENCES VICTIME(Id_Vict)
+);
+
+CREATE TABLE NAVIGUATION(
+   Id_Incident VARCHAR(50),
+   Id_Bateau VARCHAR(50),
+   PRIMARY KEY(Id_Incident, Id_Bateau),
+   FOREIGN KEY(Id_Incident) REFERENCES INCIDENT(Id_Incident),
+   FOREIGN KEY(Id_Bateau) REFERENCES BATEAU(Id_Bateau)
+);
